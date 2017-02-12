8cd8d8bf38b6decd81731a3dd4d26619bb7f6f53
Christian Silver <christian@decode.org.nz>
2017-02-12 23:45:40 +0000

    Testing

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
 import { add } from 'src/lib';
+import { World } from './world';
+
+new World();
 
 console.log(`5 + 7 is ${add(5, 7)}`);
--- src/world.ts
+++ src/world.ts
@@ -1,4 +1,8 @@
 import * as THREE from 'three';
+import 'src/vendor/three-vreffect';
+import 'src/vendor/three-vrcontrols';
+import 'webvr-polyfill';
+
 /* Uncomment once shape.ts exists:
 import { Shape } from 'src/shape.ts';*/
 
@@ -10,6 +14,8 @@ export class World {
   private scene : THREE.Scene;
   private camera : THREE.PerspectiveCamera;
   private renderer : THREE.WebGLRenderer;
+  private effect: THREE.VREffect;
+  private controls: THREE.VRControls;
 
   // Each World will also keep track of what shapes are currently in it:
   // NOTE: This is a private member.
@@ -23,13 +29,40 @@ export class World {
 
     // NOTE: arguments to perspective camera are:
     // Field of view, aspect ratio, near and far clipping plane
-    this.camera = new THREE.PerspectiveCamera( 75,
+    this.camera = new THREE.PerspectiveCamera( 200,
       window.innerWidth / window.innerHeight, 0.1, 1000 );
-    this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setSize( window.innerWidth, window.innerHeight );
 
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    console.log(THREE);
+    this.effect = new THREE.VREffect(this.renderer);
+    this.effect.setSize(window.innerWidth, window.innerHeight);
+    this.controls = new THREE.VRControls(this.camera);
+
+    const makeCube = (x: number, y: number, z: number) => {
+      const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      const cube = new THREE.Mesh( geometry, material );
+      cube.position.z = z;
+      cube.position.y = y;
+      cube.position.x = x;
+
+      this.scene.add(cube);
+    }
     // Add to HTML:
     document.body.appendChild( this.renderer.domElement );
+
+
+
+    makeCube(5, 0, 0);
+    makeCube(0, 5, 0);
+    makeCube(0, 0, 5);
+    makeCube(-5, 0, 0);
+    makeCube(0, -5, 0);
+    makeCube(0, 0, -5);
+
+
+    this.render();
 
   }
 
@@ -47,6 +80,7 @@ export class World {
   // Render world:
   render = () => {
     requestAnimationFrame( this.render );
-    this.renderer.render(this.scene, this.camera);
+    this.controls.update();
+    this.effect.render(this.scene, this.camera);
   }
 }
